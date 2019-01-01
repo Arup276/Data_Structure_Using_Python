8af24a6e17d3bd4cb649a9ff504dd0ac90b0e1ef
Arup Das <32811517+Arup276@users.noreply.github.com>
2019-01-01 12:40:15 +0530

    Update linear_search.py

--- Search_algo/linear_search.py
+++ Search_algo/linear_search.py
@@ -1,5 +1,7 @@
 def linear(a,num):
-    for i in range(len(a)):
-        if a[i] == num:
+    for i in range(len(a)): 
+        if a[i] == num: # checking searching element with every element of the array
             return "Element Found at position: {}".format(a.index(a[i]))
     return "Not Found"
+
+#Time Complexcity :- O(n)
