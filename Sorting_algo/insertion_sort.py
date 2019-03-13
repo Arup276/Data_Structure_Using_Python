761d23d4b2fc88e4c05e726cfedc32141edfdabe
Arup Das <32811517+Arup276@users.noreply.github.com>
2019-03-13 19:14:37 +0530

    Update insertion_sort.py

--- Sorting_algo/insertion_sort.py
+++ Sorting_algo/insertion_sort.py
@@ -2,7 +2,8 @@ def insertion(arr):
     for i in range(1,len(arr)):
         value = arr[i]  
         hole = i
-        while hole and arr[hole-1] > value: #Check while hole > 0 and previous value of hole is > current value
+        #`while hole and arr[hole-1] > value:` means while hole True and arr[hole-1] is greater than the value
+        while hole and arr[hole-1] > value: #Check while hole > 0 and previous value of hole is > current value 
             arr[hole] = arr[hole-1] # then take the value from prev hole and sift the hole
             hole -=1
      arr[hole] = value # At the end of while loop put the smallest elemnt in hole
