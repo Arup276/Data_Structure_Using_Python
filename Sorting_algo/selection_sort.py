0c4325c715f1d57f7eba58a839dea9917b2ca082
Arup Das <32811517+Arup276@users.noreply.github.com>
2019-03-13 18:27:37 +0530

    Update selection_sort.py

--- Sorting_algo/selection_sort.py
+++ Sorting_algo/selection_sort.py
@@ -1,9 +1,36 @@
 def selection(arr):
+    for i in range(len(arr)):
+        #print('i: ',i) to clear the confusion
+        imin = i
+        p = i+1
+        for j in range(p,len(arr)):
+            if arr[j] < arr[imin]:
+                imin = j
+            #print('j: ',j)
+        arr[i],arr[imin] = arr[imin],arr[i]
+        
+    return arr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''def selection(arr):
     limit = len(arr)
     for i in range(limit):
         for j in range(i,limit):
             if arr[j] < arr[i]:
                 arr[j],arr[i] = arr[i],arr[j]
-    return arr
+    return arr'''
 
 #Time complexcityb is: O(n^2)
