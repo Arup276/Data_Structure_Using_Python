4020fc02c3037c770de81ca1c48e1a296abfc0e5
Arup Das <32811517+Arup276@users.noreply.github.com>
2019-03-26 09:50:00 +0530

    Update quick_sort.py

--- Sorting_algo/quick_sort.py
+++ Sorting_algo/quick_sort.py
@@ -1,94 +1,34 @@
-def quicksort(arr):
-    quick(arr,0,len(arr)-1)
-
-def quick(arr,start,end):
+def quick(arr):
+    start = 0
+    end = len(arr)-1
+    #print(start,end,arr)
+    quicksort(arr,start,end)
+    
+    
+def quicksort(arr,start,end):
     if start < end:
         pindex = partition(arr,start,end)
-        quick(arr,start,pindex-1)
-        quick(arr,pindex+1,end)
-
-def partition(arr,start,end):
-    pinde = end
-    pivot = arr[pinde]
-    index = start
-    for i in range(start,end):
-        if arr[i] < pivot:
-            arr[i],arr[index] = arr[index],arr[i]
-            index = index+1
-    arr[index],arr[pinde] = arr[pinde],arr[index]
-    return index  
-
-
-
-
-
-###Not in place 
-##def quick(a):
-##    if len(a) <=1:
-##        return a
-##    smaller,equal,larger = [],[],[]
-##    pivot = a[len(a) - 1]
-##    for i in a:
-##        if i < pivot:
-##            smaller.append(i)
-##        elif i > pivot:
-##            larger.append(i)
-##        else:
-##            equal.append(i)
-##    return quick(smaller)+equal+quick(larger)
-
-
-
-
-##            temp = arr[i]
-##            arr[i] = arr[index]
-##            arr[index] = temp
+        print(pindex)
+        quicksort(arr,start,pindex-1)
+        quicksort(arr,pindex+1,end)
         
-
-
-
+def partition(arr,start,end):
+    end_ind = end
+    pivot = arr[end]
+    pindex = start
+    for i in range(start,end):
+        if arr[i] <= pivot:
+            arr[i],arr[pindex]=arr[pindex],arr[i]
+            pindex += 1
             
-##    temp = arr[index]
-##    print("arr[index]:",arr[index])
-##    print("Pivot",pivot)
-##    arr[index] = pivot
-##    arr[pinde] = arr[index]
-##    print("index",index)
-##    print(arr)
-##    return index
-##
+    arr[end_ind],arr[pindex] = arr[pindex],arr[end_ind] # pivot,arr[pindex] = arr[pindex],pivot ---this is not work because last position is not changing
+    print(arr)
+    #print('A: pindex',pindex)
+    return pindex
 
-##
-##
-##
-##
-####def quick_sort(arr):
-####    end = len(arr)-1
-####    if end > 1:
-####        pivot = arr[0]
-####        i = 0
-####        for j in range(end):
-####            if arr[j+1] < pivot:
-####                arr[j+1] ,arr[i+1] = arr[i+1],arr[j+1]
-####                i +=1
-####        arr[0], arr[i] = arr[i],arr[0]
-####        first_part = quick_sort(arr[:i])
-####        print(first_part)
-####        second_part = quick_sort(arr[i+1:])
-####        first_part.append(arr[i])
-####        return first_part+second_part
-####def quicksort(x):
-####    if len(x) == 1 or len(x) == 0:
-####        return x
-####    else:
-####        pivot = x[0]
-####        i = 0
-####        for j in range(len(x)-1):
-####            if x[j+1] < pivot:
-####                x[j+1],x[i+1] = x[i+1], x[j+1]
-####                i += 1
-####        x[0],x[i] = x[i],x[0]
-####        first_part = quicksort(x[:i])
-####        second_part = quicksort(x[i+1:])
-####        first_part.append(x[i])
-####        
+
+# Time complexity
+# Best case,average case : O(nlogn)
+# Worst case : O(n^2)
+
+# Best thing about quick sort is it's inplace sorting algoritham and space complexity is : O(1) , no extra memory needed.
